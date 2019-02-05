14bb8a038d356fa01917340367abd6f33e33011f
Eleni Chappen <echappen@Elenis-MacBook-Pro.local>
2019-02-05 11:10:22 -0600

    chg:usr: adding checks for null and indefined in numerical field handler

--- src/lib/utils/numericalFieldHandler.js
+++ src/lib/utils/numericalFieldHandler.js
@@ -4,7 +4,12 @@ const numericalFieldHandler = callback => (event) => {
   // Convert input from a string to a number
   const inputNumber = +event.target.value;
   // Check if is a number and is less than 1000
-  if (isNaN(inputNumber) || inputNumber >= 1000) return;
+  if (isNaN(inputNumber)
+  || inputNumber === null
+  || typeof(inputNumber) == 'undefined'
+  || inputNumber >= 1000) {
+    return 1;
+  }
 
   const editedEvent = { target: { value: inputNumber, name: event.target.name } };
   callback(editedEvent);
